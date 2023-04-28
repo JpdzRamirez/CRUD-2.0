b3154e0a5c174a9c3a50fc4fe0e574e4bdea0828
ramires <jeremyivanpedraza@gmail.com>
2023-04-28 14:19:18 -0500

    28-04-2023- tarde

--- src/app/home/login/login.component.ts
+++ src/app/home/login/login.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { AfterViewInit,Component } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements AfterViewInit {
+  isAllLoaded:boolean=false;
 
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      this.isAllLoaded=true;
+    }, 2200);
+
+  }
 
 
   loginCheck():void{
--- src/app/home/menu/menu.component.ts
+++ src/app/home/menu/menu.component.ts
@@ -1,19 +1,41 @@
-import { AfterViewInit, Component } from '@angular/core';
+import {  Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss']
 })
-export class MenuComponent implements AfterViewInit {
+export class MenuComponent implements OnInit  {
+  // Sets the number of stars we wish to display
 
-  isAllLoaded:boolean=false;
 
-  ngAfterViewInit(): void {
-    setTimeout(() => {
-      this.isAllLoaded=true;
-    }, 2200);
+ngOnInit(): void {
 
+  const numStars = 100;
+
+// For every star we want to display
+
+
+  for (let i = 0; i < numStars; i++) {
+    let star = document.createElement("div");
+    star.className = "star";
+    var xy = this.getRandomPosition();
+      star.style.top = xy[0] + 'px';
+      star.style.left = xy[1] + 'px';
+    document.body.append(star);
   }
+}
+
+
+
+// Gets random x, y values based on the size of the container
+public getRandomPosition() {
+  var y = window.innerWidth;
+  var x = window.innerHeight;
+    var randomX = Math.floor(Math.random()*x);
+    var randomY = Math.floor(Math.random()*y);
+    return [randomX,randomY];
+}
+
 
 }
